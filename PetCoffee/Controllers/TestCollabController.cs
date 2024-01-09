15d43878b1b7e77b62761e850d64e92e75eb613a
tainguyen58ntt <tainguyen58ntt@gmail.com>
2024-01-09 10:28:20 +0700

    Test collab

---
+++ PetCoffee/Controllers/TestCollabController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PetCoffee.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestCollabController : ApiControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
