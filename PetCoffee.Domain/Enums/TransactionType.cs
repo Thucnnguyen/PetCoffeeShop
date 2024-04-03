2ff80a0f992ad11c772fc2ec7c3b15ab0cbebbf1
THUC NGUYEN <ntthuc321@gmail.com>
2024-04-03 13:57:16 +0700

    update dev v2

--- PetCoffee.Domain/Enums/TransactionType.cs
+++ PetCoffee.Domain/Enums/TransactionType.cs
@@ -19,12 +19,15 @@ public enum TransactionType
 	[Description("Mua Quà Tặng")]
 	BuyItem = 4,
 
-    [Description("Hoàn tiền đặt chỗ")]
-    Refund = 5,
+	[Description("Hoàn tiền đặt chỗ")]
+	Refund = 5,
 	[Description("Thêm đồ ăn thức uống")]
 	AddProducts = 6,
 	[Description("Mua gói")]
 	Package = 7,
 	[Description("Hủy đặt đồ uống")]
 	RemoveProducts = 8,
+	[Description("Giảm đồ ăn thức uống")]
+	MinusProducts = 9,
 }
+
