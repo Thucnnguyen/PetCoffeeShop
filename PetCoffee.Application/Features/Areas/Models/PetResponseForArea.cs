afef74a9751841013ba50b78889a34d2495548e6
tainguyen58ntt <tainguyen58ntt@gmail.com>
2024-04-08 11:56:11 +0700

    update- get area id - get current account

--- PetCoffee.Application/Features/Areas/Models/PetResponseForArea.cs
+++ PetCoffee.Application/Features/Areas/Models/PetResponseForArea.cs
@@ -1,5 +1,4 @@
-﻿
-
+﻿using PetCoffee.Application.Features.Pet.Models;
 using PetCoffee.Domain.Enums;
 
 namespace PetCoffee.Application.Features.Areas.Models;
@@ -8,9 +7,16 @@ public class PetResponseForArea
 {
 	public long Id { get; set; }
 	public string Name { get; set; }
-
+	public int? BirthYear { get; set; }
+	public double? Weight { get; set; }
+	public decimal? Rate { get; set; } = 0;
 	public string? Avatar { get; set; }
-	public double? Rate { get; set; }
+	public string? Backgrounds { get; set; }
+	public string? Description { get; set; }
+	public PetStatus PetStatus { get; set; }
 	public PetType PetType { get; set; }
+	public PetGender Gender { get; set; }
+	public TypeSpecies TypeSpecies { get; set; }
+	public bool Spayed { get; set; } = false;
 
 }
